083e5d362876a93a8a9bd2de79699fb2ceac178d
sy2298 <sy2298@naver.com>
2022-01-01 22:49:19 +0900

    second commit

---
+++ src/main/java/com/example/firstproject/dto/DevDto.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.dto;
+
+import java.time.LocalDateTime;
+
+
+public class DevDto {
+
+    String name;
+    Integer age;
+    LocalDateTime startAt;
+
+
+}
---
+++ src/main/java/com/example/firstproject/entity/Developer.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+
+
+public class Developer {
+
+}
